67c482f15d3881fde63c95f63ce21380efb025c6
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-23 22:51:31 +0100

    First Commit #1

---
+++ app/src/main/java/com/mxcsyounes/presonaldictionary/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.mxcsyounes.presonaldictionary
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/mxcsyounes/presonaldictionary/database/entities/Word.kt
@@ -0,0 +1,6 @@
+package com.mxcsyounes.presonaldictionary.database.entities
+
+import java.util.*
+
+
+class Word(val id: Int, val date: Date, val word: String, val definition: String)
